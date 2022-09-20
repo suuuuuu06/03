67b498a4f2264f00f2d852cea4e96435c839eb2e
suuuuuu06 <bsj06@sookmyung.ac.kr>
2022-09-20 17:40:58 +0900

    01-1
    실수해서 다시 했습니다.

--- main.c
+++ main.c
@@ -4,18 +4,18 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(void) {
-	int a;
-	int b;
-	
-	printf("분모를 입력하세요 : ");
-	scanf("%d", &a);
-	
-	printf("분자를 입력하세요 : ");
-	scanf("%d", &b);
-	
-	printf("나누기의 결과는 %f 입니다.",(float)a/b);
-	
-	return 0;
+	int input_int;
+	float input_float;
+
+	printf("enter an integer : ");
+	scanf("%d", &input_int);
+
+	printf("enter a flaot : ");
+	scanf("%f", &input_float);
+
+	printf("integer : %d, float : %f\n", input_int, input_float);
+
+    return 0;
 	
 	
 }
